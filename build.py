075a07e8e3201eb396fadd56e6e2e7887c3001a3
Raymond Ehlers <raymond.ehlers@gmail.com>
2020-10-07 17:38:33 +0200

    A number of fixes and improvements for building
    Still needs cleanup, etc. But now it can be built via PEP 517!

--- build.py
+++ build.py
@@ -4,16 +4,19 @@ Adapted from https://github.com/pybind/cmake_example
 import os
 import platform
 import re
+import shutil
 import subprocess
 import sys
 import sysconfig
+from distutils.command.build_ext import build_ext
+from distutils.core import Distribution, Extension
 from distutils.version import LooseVersion
 from typing import Any, Dict
 
-from numpy import get_include as get_numpy_include
-from setuptools.command.build_ext import build_ext
-from setuptools.extension import Extension
-from setuptools import find_packages
+#from numpy import get_include as get_numpy_include
+#from setuptools.command.build_ext import build_ext
+#from setuptools.extension import Extension
+#from setuptools import find_packages
 
 
 class CMakeExtension(Extension):
@@ -36,6 +39,7 @@ class ExtensionBuilder(build_ext):
             super().build_extension(ext)
 
     def validate_cmake(self) -> None:
+        print("About to run validation")
         cmake_extensions = [x for x in self.extensions if isinstance(x, CMakeExtension)]
         if len(cmake_extensions) > 0:
             try:
@@ -51,6 +55,7 @@ class ExtensionBuilder(build_ext):
                     raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
     def build_cmake_extension(self, ext: CMakeExtension) -> None:
+        print("About to build")
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir, "-DPYTHON_EXECUTABLE=" + sys.executable]
 
@@ -83,7 +88,19 @@ class ExtensionBuilder(build_ext):
         #cmake_args += [f'-DGMP=/Users/re239/alice/sw/osx_x86-64/GMP/v6.0.0-1/']
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        subprocess.run(["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env, check=True)
+        print("Configurtion...")
+        print(f"Source dir: {ext.sourcedir} with: {os.listdir(ext.sourcedir)}")
+        print(f"self.build_temp: {self.build_temp} with {os.listdir(self.build_temp)}")
+        print(f"cwd: {os.getcwd()}")
+        # TODO: Maybe glob this directory?
+        # Somehow, I need to run the cwd from the build dir, but scan the package directory.
+        #subprocess.run(["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env, check=True)
+        # Source directory = -S, build directory = -B
+        # cmake -S . -B build
+        # cmake --build build
+        #subprocess.run(["cmake", "-S", ext.sourcedir] + cmake_args, cwd=ext.sourcedir, env=env, check=True)
+        subprocess.run(["cmake", "-S", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env, check=True)
+        print("Building...")
         subprocess.run(["cmake", "--build", "."] + build_args, cwd=self.build_temp, check=True)
 
 
@@ -95,9 +112,33 @@ def build(setup_kwargs: Dict[str, Any]) -> None:
     #   - https://github.com/python-poetry/poetry/issues/2740
     cmake_modules = [CMakeExtension("pyfastjet._src")]
     ext_modules = cmake_modules
-    setup_kwargs.update({
-        "packages": find_packages(),
-        "ext_modules": ext_modules,
-        "cmdclass": dict(build_ext=ExtensionBuilder),
-        "zip_safe": False,
-    })
+    #setup_kwargs.update({
+    #    "packages": find_packages(),
+    #    "ext_modules": ext_modules,
+    #    "cmdclass": dict(build_ext=ExtensionBuilder),
+    #    "zip_safe": False,
+    #})
+
+    distribution = Distribution({"name": "pyfastjet", "ext_modules": ext_modules})
+    distribution.package_dir = "pyfastjet"
+
+    # More or less directly from https://github.com/python-poetry/poetry/issues/2740
+    cmd = ExtensionBuilder(distribution)
+    cmd.ensure_finalized()
+    cmd.run()
+
+    # Copy built extensions back to the project
+    for output in cmd.get_outputs():
+        relative_extension = os.path.relpath(output, cmd.build_lib)
+        print(f"Copying {output} to {relative_extension}")
+        shutil.copyfile(output, relative_extension)
+        mode = os.stat(relative_extension).st_mode
+        mode |= (mode & 0o444) >> 2
+        os.chmod(relative_extension, mode)
+
+    #sys.exit(1)
+
+    return setup_kwargs
+
+if __name__ == "__main__":
+    build({})
